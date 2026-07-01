d81e134c6b8ac46d2b388e345ba49eae78d69a01
Yuri <isyuricunha@duck.com>
2026-07-01 18:24:03 -0300

    fix(ci): clone immutable proxy object in transform to prevent crash in release notes generator

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -19,50 +19,51 @@ module.exports = {
           transform: (commit, context) => {
             let discard = true;
             const issues = [];
+            const c = Object.assign({}, commit);
             
-            if (commit.type === 'feat') {
-              commit.type = 'Features';
+            if (c.type === 'feat') {
+              c.type = 'Features';
               discard = false;
-            } else if (commit.type === 'fix') {
-              commit.type = 'Bug Fixes';
+            } else if (c.type === 'fix') {
+              c.type = 'Bug Fixes';
               discard = false;
-            } else if (commit.type === 'perf') {
-              commit.type = 'Performance Improvements';
+            } else if (c.type === 'perf') {
+              c.type = 'Performance Improvements';
               discard = false;
-            } else if (commit.type === 'revert' || commit.revert) {
-              commit.type = 'Reverts';
+            } else if (c.type === 'revert' || c.revert) {
+              c.type = 'Reverts';
               discard = false;
-            } else if (commit.type === 'refactor') {
-              commit.type = 'Code Refactoring';
+            } else if (c.type === 'refactor') {
+              c.type = 'Code Refactoring';
               discard = false;
-            } else if (commit.type === 'audit') {
-              commit.type = 'Security Audits';
+            } else if (c.type === 'audit') {
+              c.type = 'Security Audits';
               discard = false;
             }
 
             if (discard) return undefined;
 
-            if (commit.scope === '*') {
-              commit.scope = '';
+            if (c.scope === '*') {
+              c.scope = '';
             }
 
-            if (typeof commit.hash === 'string') {
-              commit.shortHash = commit.hash.substring(0, 7);
+            if (typeof c.hash === 'string') {
+              c.shortHash = c.hash.substring(0, 7);
             }
 
-            if (typeof commit.subject === 'string') {
+            if (typeof c.subject === 'string') {
               let url = context.repository
                 ? `${context.host}/${context.owner}/${context.repository}`
                 : context.repoUrl;
               if (url) {
                 url = `${url}/issues/`;
-                commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+                c.subject = c.subject.replace(/#([0-9]+)/g, (_, issue) => {
                   issues.push(issue);
                   return `[#${issue}](${url}${issue})`;
                 });
               }
               if (context.host) {
-                commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+                c.subject = c.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
                   if (username.includes('/')) {
                     return `@${username}`;
                   }
@@ -71,11 +72,11 @@ module.exports = {
               }
             }
 
-            commit.references = commit.references.filter(reference => {
+            c.references = (c.references || []).filter(reference => {
               return issues.indexOf(reference.issue) === -1;
             });
 
-            return commit;
+            return c;
           }
         }
       }
